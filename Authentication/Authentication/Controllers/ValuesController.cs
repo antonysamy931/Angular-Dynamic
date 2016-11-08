87666b20cc051d4881bf7c8280080703a9adec8b
unknown <Antony_S@ravsoftw0252.MEDUSA.LOCAL>
2016-11-08 20:22:14 +0530

    ip address

--- Authentication/Authentication/Controllers/ValuesController.cs
+++ Authentication/Authentication/Controllers/ValuesController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Security.Permissions;
+using System.ServiceModel.Channels;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 
@@ -38,6 +40,34 @@ namespace Authentication.Controllers
         // DELETE api/values/5
         public void Delete(int id)
         {
+        }
+
+        public string GetIp()
+        {
+            return GetClientIp();
+        }
+
+        private string GetClientIp(HttpRequestMessage request = null)
+        {
+            request = request ?? Request;
+
+            if (request.Properties.ContainsKey("MS_HttpContext"))
+            {
+                return ((HttpContextWrapper)request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+            }
+            else if (request.Properties.ContainsKey(RemoteEndpointMessageProperty.Name))
+            {
+                RemoteEndpointMessageProperty prop = (RemoteEndpointMessageProperty)request.Properties[RemoteEndpointMessageProperty.Name];
+                return prop.Address;
+            }
+            else if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Request.UserHostAddress;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
